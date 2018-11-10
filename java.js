493530d6ced422ab1819edfa0899e4d6e1a18527
lrllzzr <43448423+lrllzzr@users.noreply.github.com>
2018-11-10 23:06:00 +0900

    first

---
+++ java.js
@@ -0,0 +1,7 @@
+var lists=['Julian hough','Hannah Wilson','Howard Stern','Bumble Bee tuna death','Surface Pro3','Mother\'s Day Gifts','Khloe Kardashian',' Uber car service','Asperger\'s Syndrome','Surface Pro3','Robin Roberts']
+var aside_ad = {
+  list : function(i){
+    document.write('<div id="aside_search_list">'+(i+1)+'. '+lists[i]+'</div>')
+  }
+
+}
